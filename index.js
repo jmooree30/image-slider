308bf011d3c9a4d167e778b601479668df828681
Jacob <jmooree30@gmail.com>
2018-02-04 23:42:46 -0500

    why single threaded

--- index.js
+++ index.js
@@ -4,53 +4,76 @@ let counter = 1;
 let container = document.querySelector(".container");
 
 arrowLeft.addEventListener("click", function() {
-  clearInterval(start)
+  clearInterval(start);
   if (counter < 1) {
     counter = 3;
   }
   if (counter == 1) {
-    container.style.backgroundImage = "url('3.jpg')";
+    container.style.opacity = "0";
+    setTimeout(function() {
+      container.style.backgroundImage = "url('3.jpg')";
+      container.style.opacity = "1";
+    }, 1010);
   }
   if (counter == 2) {
-    container.style.backgroundImage = "url('1.jpg')";
+    container.style.opacity = "0";
+    setTimeout(function() {
+      container.style.backgroundImage = "url('2.png')";
+      container.style.opacity = "1";
+    }, 1010);
   }
   if (counter == 3) {
-    container.style.backgroundImage = "url('2.png')";
+    container.style.opacity = "0";
+    setTimeout(function() {
+      container.style.backgroundImage = "url('1.jpg')";
+      container.style.opacity = "1";
+    }, 1010);
   }
   counter -= 1;
 });
 
 arrowRight.addEventListener("click", function() {
-  clearInterval(start)
+  clearInterval(start);
   container = document.querySelector(".container");
   if (counter > 3) {
     counter = 1;
     imageIndex = 0;
   }
   if (counter == 1) {
-    container.style.backgroundImage = "url('2.png')";
+    container.style.opacity = "0";
+    setTimeout(function() {
+      container.style.backgroundImage = "url('2.png')";
+      container.style.opacity = "1";
+    }, 1010);
   }
   if (counter == 2) {
-    container.style.backgroundImage = "url('3.jpg')";
+    container.style.opacity = "0";
+    setTimeout(function() {
+      container.style.backgroundImage = "url('3.jpg')";
+      container.style.opacity = "1";
+    }, 1010);
   }
   if (counter == 3) {
-    container.style.backgroundImage = "url('1.jpg')";
+    container.style.opacity = "0";
+    setTimeout(function() {
+      container.style.backgroundImage = "url('3.jpg')";
+      container.style.opacity = "1";
+    }, 1010);
   }
   counter += 1;
 });
 
-var imageArray = ["2.png","3.jpg","1.jpg"];
+var imageArray = ["2.png", "3.jpg", "1.jpg"];
 var imageIndex = 0;
- 
+
 function changeImage() {
-   container.style.backgroundImage = `url(${imageArray[imageIndex]})`;
-   container.style.opacity = "1"
-   imageIndex++;
-   if (imageIndex >= imageArray.length) {
-      imageIndex = 0;
-   }
+  container.style.backgroundImage = `url(${imageArray[imageIndex]})`;
+  imageIndex++;
+  if (imageIndex >= imageArray.length) {
+    imageIndex = 0;
+  }
 }
- 
+
 window.onload = function() {
-   start = setInterval(changeImage,100);
-}
+  start = setInterval(changeImage, 2000);
+};
